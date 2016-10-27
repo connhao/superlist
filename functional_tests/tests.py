33ee794e55cde6ac1848827f75f30be92cfb8110
jbjbc <jbjbc>
2016-10-27 23:50:22 +0800

    add test for unique url of user's to-do lists,test for user's cookie

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,13 +38,16 @@ class NewVisitorTest(LiveServerTestCase):
         #她按回车键后，页面更新了
         #待办事项表格中显示了“1：Buy peacock feathers” 
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url 
+        self.assertRegex(edith_list_url, '/lists/.+') 
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         import time 
         time.sleep(2)
 
-        table = self.browser.find_element_by_id('id_list_table') 
-        rows = table.find_elements_by_tag_name('tr') 
+        # table = self.browser.find_element_by_id('id_list_table') 
+        # rows = table.find_elements_by_tag_name('tr') 
 
-        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        # self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
 
         #页面中又显示了一个文本框，可以输入其它待办事项 
         #她输入了“Use peacock feathers to  make a fly”(使用孔雀羽毛做假蝇)
@@ -56,27 +59,39 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(3)
 
         #页面再次更新，她的清单中显示了两个待办事项
-        table = self.browser.find_element_by_id('id_list_table') 
-        rows = table.find_elements_by_tag_name('tr') 
-        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
+        # table = self.browser.find_element_by_id('id_list_table') 
+        # rows = table.find_elements_by_tag_name('tr') 
+        # self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        # self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
+        self.check_for_row_in_list_table('2: Use peacock feathres to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers') 
+
+        #现在一个叫做弗朗西斯的新用户访问了网站
+        ##我们使用一个新浏览器会话
+        ##确保伊迪丝的信息不会从cookie中泄漏出来 
+        self.browser.get(self.live_server_url) 
+        page_text = self.browser.find_element_by_tag_name('body').text 
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertNotIn('make a fly',page_text)
+
+        #弗朗西斯输入一个新待办事项，新建一个清单 
+        #他不想伊迪丝那样新趣盎然
+        inputbox = self.browser.find_elements_by_id('id_new_item') 
+        inputbox.send_keys('Buy milk') 
+        inputbox.send_keys(Keys.ENTER)  
+
+        #弗朗西斯获得了他的唯一URL
+        francis_list_url = self.browser.current_url 
+        self.assertRegex(francis_list_url, '/lists/.+') 
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        #这个页面还是没有edity的清单 
+        page_text = self.browser.find_element_by_tag_name('body').text 
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertNotIn('make a fly',page_text)
 
 
-        #伊迪丝想知道这个网站是否会记住她的清单 
-
-        #她看到网站为她生成了唯一的URL 
-        #而且页面中有一些文字解说这个功能 
-        self.fail('Finish the test!') 
-
-        #她访问那个URL，发现他的待办事项列表还在
-        #她很满意，去睡觉了 
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
-
-
-    
+        #两人都很满意，去睡觉了 
 
 
 
