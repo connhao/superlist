d52133d3e03bf2f23727ddd98799f94430bdfe20
jbjbc <jbjbc>
2016-10-28 18:37:16 +0800

    add db table List

--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 from django.http import HttpRequest 
 from django.template.loader import render_to_string 
 
-from lists.views import home_page,view_list
-from lists.models import Item 
+from lists.views import home_page, view_list
+from lists.models import Item, List
 
 # Create your tests here.
 
@@ -31,14 +31,14 @@ class HomePageTest(TestCase):
     #     home_page(request)
     #     self.assertEqual(Item.objects.count(), 0)
 
-    def test_home_page_display_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        request = HttpRequest()
-        response = view_list(request)
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+    # def test_home_page_display_all_list_items(self):
+    #     Item.objects.create(text='itemey 1')
+    #     Item.objects.create(text='itemey 2')
+    #     request = HttpRequest()
+    #     response = view_list(request)
+    #
+    #     self.assertIn('itemey 1', response.content.decode())
+    #     self.assertIn('itemey 2', response.content.decode())
 
 
 class ListViewTest(TestCase):
@@ -48,7 +48,14 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        pass
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
+
+        respose = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(respose, 'itemey 1')
+        self.assertContains(respose, 'itemey 2')
 
     def test_save_a_POST_request(self):
         self. client.post(
@@ -77,23 +84,33 @@ class ListViewTest(TestCase):
         #     )
         # self.assertEqual(response.content.decode(), expected_html)
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item() 
-        first_item.text = 'The first (ever) list item' 
+        first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save() 
 
         second_item = Item() 
-        second_item.text = 'Item the second' 
+        second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save() 
-        
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
         saved_items = Item.objects.all() 
         self.assertEqual(saved_items.count(), 2) 
 
         first_saved_item = saved_items[0] 
         second_saved_item = saved_items[1] 
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item') 
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 
